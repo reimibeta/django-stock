af1291decd5f2e2e0387a57186dff2335b026836
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-03-02 17:16:07 +0700

    Update stock_algorithm.py

--- stock_algorithms/stock_algorithm.py
+++ stock_algorithms/stock_algorithm.py
@@ -1,25 +1,29 @@
 class StockAlgorithm:
 
     @classmethod
-    def supply_stock(cls, current_stock, new_stock, old_stock=None):
-        if old_stock is None:
-            return current_stock + new_stock
-        else:
-            return current_stock - old_stock + new_stock
+    def supply_stock_new(cls, current_stock, new_stock):
+        return current_stock + new_stock
 
     @classmethod
-    def outlet_stock(cls, current_stock, new_stock, old_stock=None):
-        if old_stock is None:
-            return current_stock - new_stock
-        else:
-            return current_stock + old_stock - new_stock
+    def supply_stock_old(cls, current_stock, new_stock, old_stock):
+        return current_stock - old_stock + new_stock
+
+    @classmethod
+    def outlet_stock_new(cls, current_stock, new_stock):
+        return current_stock - new_stock
+
+    @classmethod
+    def outlet_stock_old(cls, current_stock, new_stock, old_stock):
+        return current_stock + old_stock - new_stock
 
     """ refund stock to store """
+
     @classmethod
     def refund_stock(cls, current_stock, old_stock):
         return current_stock + old_stock
 
     """ return stock to supplier """
+
     @classmethod
     def return_stock(cls, current_stock, old_stock):
         return current_stock - old_stock
